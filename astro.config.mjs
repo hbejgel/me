bebf4daa318a5282cce4cd0c08ed128ec5ff2ec1
Henrique Bejgel <hbejgel@gmail.com>
2025-07-26 18:17:04 -0300

    Correcting relative paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://hbejgel.github.io/me",
+    base: "/me",
+});
